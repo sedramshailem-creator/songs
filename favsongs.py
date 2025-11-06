3fd50466bf3ff872d1104a394cd24391aed7ee21
doaa <doaasalama1715@gmail.com>
2025-11-06 11:53:51 +0200

    add task

--- favsongs.py
+++ favsongs.py
@@ -1,5 +1,12 @@
-favorite_songs = ["perfect" , "until i found you", "senorita", "diamonds"]
+# favorite_songs = ["perfect" , "until i found you", "senorita", "diamonds"]
 
-print(favorite_songs[0])
-print(favorite_songs[1])
-print(favorite_songs[2])
+# print(favorite_songs[0])
+# print(favorite_songs[1])
+# print(favorite_songs[2])
+
+
+songs = ["perfect", "shape of you", "someone like you", "sweet"]
+print ("here are my favorite songs ")
+
+for songs in songs:
+    print(songs)
